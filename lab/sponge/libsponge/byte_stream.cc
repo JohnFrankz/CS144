7d6d1fa92ca023342e2cf339ab9e1d084b8c5c90
JohnFrankz <86246881+JohnFrankz@users.noreply.github.com>
2023-02-18 08:41:44 +0000

    finish lab 0

---
+++ lab/sponge/libsponge/byte_stream.cc
@@ -0,0 +1,67 @@
+#include "byte_stream.hh"
+
+// Dummy implementation of a flow-controlled in-memory byte stream.
+
+// For Lab 0, please replace with a real implementation that passes the
+// automated checks run by `make check_lab0`.
+
+// You will need to add private members to the class declaration in `byte_stream.hh`
+
+template <typename... Targs>
+void DUMMY_CODE(Targs &&... /* unused */) {}
+
+using namespace std;
+
+ByteStream::ByteStream(const size_t c): 
+    _capacity(c),
+    _readBytes(0),
+    _writeBytes(0),
+    _buffer(),
+    _end()
+    {}
+
+size_t ByteStream::write(const string &data) {
+    size_t writableSize = std::min(data.size(), remaining_capacity());
+    _buffer.insert(_buffer.end(), data.begin(), data.begin() + writableSize);
+    _writeBytes += writableSize;
+    return writableSize;
+}
+
+//! \param[in] len bytes will be copied from the output side of the buffer
+string ByteStream::peek_output(const size_t len) const {
+    size_t peekSize = std::min(len, _buffer.size());
+    return std::string(_buffer.begin(), _buffer.begin() + peekSize);
+}
+
+//! \param[in] len bytes will be removed from the output side of the buffer
+void ByteStream::pop_output(const size_t len) {
+    size_t popSize = std::min(len, _buffer.size());
+    _buffer.erase(_buffer.begin(), _buffer.begin() + popSize);
+    _readBytes += popSize;
+}
+
+//! Read (i.e., copy and then pop) the next "len" bytes of the stream
+//! \param[in] len bytes will be popped and returned
+//! \returns a string
+std::string ByteStream::read(const size_t len) {
+    size_t readSize = std::min(len, _buffer.size());
+    std::string res(_buffer.begin(), _buffer.begin() + readSize);
+    pop_output(len);
+    return res;
+}
+
+void ByteStream::end_input() { _end = true;}
+
+bool ByteStream::input_ended() const { return _end; }
+
+size_t ByteStream::buffer_size() const { return _buffer.size(); }
+
+bool ByteStream::buffer_empty() const { return _buffer.empty(); }
+
+bool ByteStream::eof() const { return _buffer.empty() && _end; }
+
+size_t ByteStream::bytes_written() const { return _writeBytes; }
+
+size_t ByteStream::bytes_read() const { return _readBytes; }
+
+size_t ByteStream::remaining_capacity() const { return _capacity - _buffer.size(); }
